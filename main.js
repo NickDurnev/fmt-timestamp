04c8c47945bb1c6c1fd5aa9ad0fb4641f9ab95d2
Никита Дурнев <nikitadurnev@Nikitas-Air.lan>
2023-04-08 10:31:33 +0300

    feat: update files

---
+++ main.js
@@ -0,0 +1,8 @@
+// lib/main.js
+import { DateFormatter } from "./src/date-formatter";
+import {
+  formatRelativeTime,
+  formatToShort,
+  formatToTime,
+} from "./src/formatData";
+export { DateFormatter, formatRelativeTime, formatToShort, formatToTime };
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from "vitest/config";
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  test: {
-    includeSource: ["src/**/*.{js,ts}"],
-    environment: "jsdom",
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "./main.js"),
+      name: "date-formatter",
+      // the proper extensions will be added
+      fileName: "date-formatter",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["lit", "@web/dev-server-esbuild"],
+      output: {},
+    },
   },
 });
