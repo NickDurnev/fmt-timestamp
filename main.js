dfb11c5d42eba432224261acd03a39c2f31830de
Никита Дурнев <nikitadurnev@Nikitas-Air.lan>
2023-04-09 12:08:36 +0300

    update package.json

---
+++ main.js
@@ -0,0 +1,9 @@
+import { css, html, LitElement } from "lit";
+import {
+  customElement,
+  property,
+  state,
+  queryAssignedNodes,
+} from "lit/decorators.js";
+import date_formatter from "https://unpkg.com/@nick_durnev/time-formatter@1.0.4";
+console.log(formatToRelative("2023-03-29T12:25:07.427Z"));
